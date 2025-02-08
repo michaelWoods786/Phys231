c1bca632651f6f7cfb08fd23a36911b055a2fb3c
michaelWoods786 <michaelawoods786@gmail.com>
2025-02-08 17:34:28 -0500

    Add files via upload

--- BlinkLab.ino
+++ BlinkLab.ino
@@ -31,6 +31,22 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+void TurnOnOffSameTime(){
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+int x = 1;
+while (x == 1){
+
+digitalWrite(6, HIGH);
+//console.log("printing 6 high, 7 off");
+delay(40);
+digitalWrite(6, LOW);
+digitalWrite(7, HIGH);
+delay(40);
+digitalWrite(7, LOW);
+}
+}
+
 void blinkFoo( int mode){
   //goes from l3 to l7
   if (mode == 0){
@@ -52,6 +68,5 @@ void blinkFoo( int mode){
 
 
 void loop() {
-  blinkFoo(0);
-  blinkFoo(1);
+TurnOnOffSameTime();
 }
